2bc9e06e8c1448a114dc96edb590fb82121fa206
Edgar Rivero <riveroedgar.jm@gmail.com>
2023-02-09 13:58:51 -0500

    Dashboard add

---
+++ first-next/pages/About.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import Layout from "@/components/layout";
+
+export default function About() {
+  return (
+    <div>
+        About
+        <Link href="/">Inicio</Link>
+    </div>
+  );
+}
